087057c570ec86e909d68a31ab69b8a4a76e5174
MShakhabov <ya.shahabov1@gmail.com>
2023-11-22 23:17:41 +0300

    доработан вывод одного тура

--- src/components/Tours/OneTour.tsx
+++ src/components/Tours/OneTour.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getOneTour, getTours } from "../../features/toursSlice";
+import { getTours } from "../../features/toursSlice";
 import { useParams } from "react-router-dom";
 import styles from "./Tours.module.css";
+import Header from "../Header/Header";
 
 const OneTour = () => {
   const dispatch = useDispatch();
@@ -14,80 +15,87 @@ const OneTour = () => {
   useEffect(() => {
     dispatch(getTours());
   }, [dispatch]);
-
+  console.log(oneTour);
   return (
-    <div className={styles.oneTour} key={oneTour?._id}>
-      <h2 className={styles.tourName}>{oneTour?.name}</h2>
+    <>
       <img
-        className={styles.tourImage}
+        className={styles.video}
         src={`http://localhost:3077/${oneTour?.image}`}
         alt=""
       />
-      <div className={styles.tourText}>
-        <div className={styles.tourDescr}>{oneTour?.descr}</div>
-        <div className={styles.tourDescr}>{oneTour?.transport}</div>
-        <div className={styles.tourDescr}>
-          Продолжительность: {oneTour?.length} дня
-        </div>
-        <div className={styles.tourPriceAndRating}>
-          <div className={styles.tourPrice}>
-            Цена: {oneTour?.price} рублей / 1 человек
+      <div className={styles.oneTour} key={oneTour?._id}>
+        <div className={styles.oneTourHead}>
+          <video autoPlay loop className={styles.oneTourImage}>
+            <source src={`http://localhost:3077/${oneTour?.video}`} />
+          </video>
+          <div className={styles.oneTourText}>
+            <h2 className={styles.oneTourName}>{oneTour?.name} </h2>
+            <div className={styles.oneTourDescr}>{oneTour?.descr}</div>
+            <div className={styles.oneTourTransport}>{oneTour?.transport}</div>
+            <div className={styles.oneTourLength}>
+              Продолжительность: {oneTour?.length} дня
+            </div>
+            <div className={styles.oneTourPriceAndRating}>
+              <div className={styles.oneTourPrice}>
+                Цена: {oneTour?.price} рублей / 1 человек
+              </div>
+              <div className={styles.oneTourRating}>
+                Оценка тура: {oneTour?.rating} ⭐
+              </div>
+            </div>
           </div>
-          <div className={styles.tourRating}>
-            Оценка тура: {oneTour?.rating} ⭐
+        </div>
+        <h1>Программа тура</h1>
+        <ul>
+          <li>
+            <h2>1 День</h2>
+            {oneTour?.firstDay}
+          </li>
+          <li>
+            <h2>2 День</h2>
+            {oneTour?.secondDay}
+          </li>
+          <li>
+            <h2>3 День</h2>
+            {oneTour?.thirdDay}
+          </li>
+        </ul>
+        <h1>Расписание по дням</h1>
+        <div className={styles.oneTourDay}>
+          <div className={styles.oneTourDayText}>
+            <h2>1 День</h2>
+            {oneTour?.firstDayDescr}
+          </div>
+          <img
+            className={styles.oneTourDayImage}
+            src={`http://localhost:3077/${oneTour?.firstDayImg}`}
+            alt=""
+          />
+        </div>
+        <div className={styles.oneTourDay}>
+          <img
+            className={styles.oneTourDayImage}
+            src={`http://localhost:3077/${oneTour?.secondDayImg}`}
+            alt=""
+          />
+          <div className={styles.oneTourDayText}>
+            <h2>2 День</h2>
+            {oneTour?.secondDayDescr}
           </div>
         </div>
-      </div>
-      <h1>Программа тура</h1>
-      <ul>
-        <li>
-          <h2>1 День</h2>
-          {oneTour?.firstDay}
-        </li>
-        <li>
-          <h2>2 День</h2>
-          {oneTour?.secondDay}
-        </li>
-        <li>
-          <h2>3 День</h2>
-          {oneTour?.thirdDay}
-        </li>
-      </ul>
-      <h1>Расписание по дням</h1>
-      <div className={styles.oneTourDay}>
-        <div className={styles.oneTourDayText}>
-          <h2>1 День</h2>
-          {oneTour?.firstDayDescr}
-        </div>
-        <img
-          className={styles.oneTourDayImage}
-          src={`http://localhost:3077/${oneTour?.firstDayImg}`}
-          alt=""
-        />
-      </div>
-      <div className={styles.oneTourDay}>
-        <img
-          className={styles.oneTourDayImage}
-          src={`http://localhost:3077/${oneTour?.secondDayImg}`}
-          alt=""
-        />
-        <div className={styles.oneTourDayText}>
-          <h2>2 День</h2>
-          {oneTour?.secondDayDescr}
+        <div className={styles.oneTourDay}>
+          <div className={styles.oneTourDayText}>
+            <h2>3 День</h2>
+            {oneTour?.thirdDayDescr}
+          </div>
+          <img
+            className={styles.oneTourDayImage}
+            src={`http://localhost:3077/${oneTour?.thirdDayImg}`}
+            alt=""
+          />
         </div>
       </div>
-      <div className={styles.oneTourDay}>
-        <div className={styles.oneTourDayText}>
-          <h2>3 День</h2>
-          {oneTour?.thirdDayDescr}
-        </div>
-        <img
-          className={styles.oneTourDayImage}
-          src={`http://localhost:3077/${oneTour?.thirdDayImg}`}
-          alt=""
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
